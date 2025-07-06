9b1212274ce4128ec790e0f003071a31d059cdf2
shadabkhaneth <shadabsheik828@gmail.com>
2025-07-06 13:01:59 +0530

    Add files via upload

---
+++ To-Do-List/script.js
@@ -0,0 +1,23 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let taskText = input.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.textContent = taskText;
+
+  let delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.onclick = function() {
+    li.remove();
+  };
+
+  li.appendChild(delBtn);
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = ""; // clear input
+}
